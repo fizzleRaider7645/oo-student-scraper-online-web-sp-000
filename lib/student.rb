ebd775d4c77238ffc6e437679c0db20a7ff95a54
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-20 19:53:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
   def add_student_attributes(attributes_hash)
     p attributes_hash
     attributes_hash.each do |key, value|
-      p self.send(("#{key}="), value)
+      self.send(("#{key}="), value)
     end
   end
 
